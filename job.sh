449d49d9f284a3a7fe973e311317c518be4b662e
Thekettlemaker <pjkazanj@bhcc.mass.edu>
2020-08-17 07:01:56 -0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -5,6 +5,8 @@
 #
 _f100=15
 _current=0
+_spin="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+i=0
 echo ""
 printf '\e[1;34m%-6s\e[m' "Spawning Containers"
 echo "
@@ -16,23 +18,29 @@ ctrl_c () {
    tput cnorm
    stty echo
    echo "
-"
+   "
    exit $?
 }
-docker-topo --create my-topology.yml > /dev/null 2>&1 &
 ProgressBar () {
-    _percent=$(("${1}*100/${_f100}*100"/100))
-    _progress=$(("${_percent}*4"/10))
-    _remainder=$((40-_progress))
-    _completed=$(printf "%${_progress}s")
-    _left=$(printf "%${_remainder}s")
-    printf "\rProgress : [${_completed// /#}${_left// /-}] ${_percent}%%"
+   _percent=$(("${1}*100/${_f100}*100"/100))
+   _progress=$(("${_percent}*4"/10))
+   _remainder=$((40-_progress))
+   _completed=$(printf "%${_progress}s")
+   _left=$(printf "%${_remainder}s")
+   printf "\rProgress : [\e[42m\e[30m${_completed// /#}\e[0m${_cspin}${_left// /-}] ${_percent}%%"
 }
 while [ "${_current}" -lt "${_f100}" ]
 do
-    sleep 1
-    _current=$(docker ps -q | wc -l)
-    ProgressBar "${_current}"
+   sleep 1
+   _current=$(wc -l < count)
+    if [ "${_current}" = "${_f100}" ]
+      then
+       _spin="#"
+       _cspin="\e[42m\e[30m${_spin:i++%${#_spin}:1}\e[0m"
+      else
+       _cspin="${_spin:i++%${#_spin}:1}"    
+    fi
+      ProgressBar "${_current}"
 done
 stty echo
 tput cnorm
