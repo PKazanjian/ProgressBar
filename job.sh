af5af58a17d675d954cb0736399fc87f8bfe0e16
Thekettlemaker <pjkazanj@bhcc.mass.edu>
2020-08-18 21:12:03 -0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -4,9 +4,9 @@
 # Ack: ProgressBar function, fork of Teddy Skarin * https://github.com/fearside/ProgressBar/ *
 #
 _f100=15
-_current=0
+_current=0 i=0
 _spin="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
-i=0
+declare a b
 echo ""
 printf '\e[1;34m%-6s\e[m' "Spawning Containers"
 echo "
@@ -25,7 +25,7 @@ ProgressBar () {
    _remainder=$((40-_progress))
    _completed=$(printf "%${_progress}s")
    _left=$(printf "%${_remainder}s")
-   printf "\rProgress : [\e[42m\e[30m${_completed// /#}\e[0m${_cspin}${_left// /-}] ${_percent}%%"
+   printf "\rProgress : [\e[42m\e[30m${_completed// /#}\e[0m${a}${_spin:i++%${#_spin}:1}${b}${_left// /-}] ${_percent}%%"
 }
 while [ "${_current}" -lt "${_f100}" ]
 do
@@ -34,9 +34,8 @@ do
     if [ "${_current}" = "${_f100}" ]
       then
        _spin="#"
-       _cspin="\e[42m\e[30m${_spin:i++%${#_spin}:1}\e[0m"
-      else
-       _cspin="${_spin:i++%${#_spin}:1}"    
+       a="\e[42m\e[30m"
+       b="\e[0m"  
     fi
       ProgressBar "${_current}"
 done
